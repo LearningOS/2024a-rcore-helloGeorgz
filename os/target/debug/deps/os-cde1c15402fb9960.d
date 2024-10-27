4797c641f310df57d7d12a032d054bffe9e88b3c
zhihao <1038959884@qq.com>
2024-10-27 11:12:38 +0800

    first day

---
+++ os/target/debug/deps/os-cde1c15402fb9960.d
@@ -0,0 +1,5 @@
+/home/zhihao/code/2024a-rcore-helloGeorgz/os/target/debug/deps/os-cde1c15402fb9960: src/main.rs
+
+/home/zhihao/code/2024a-rcore-helloGeorgz/os/target/debug/deps/os-cde1c15402fb9960.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/debug/os.d
@@ -0,0 +1,1 @@
+/home/zhihao/code/2024a-rcore-helloGeorgz/os/target/debug/os: /home/zhihao/code/2024a-rcore-helloGeorgz/os/src/main.rs
